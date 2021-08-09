c7f11c8f2b04435e3233fdd5fa3811cc4fc2f993
Mayank Parasar <mparasar3@gatech.edu>
2021-08-08 20:44:10 -0400

    Re-structuring the BINDU codebase

--- gem5/run_script64c.sh
+++ gem5/run_script64c.sh
@@ -27,7 +27,7 @@ do
 # for k in 0.02 0.04 0.06 0.08 0.10 0.12 0.14 0.16 0.18 0.20 0.22 0.24 0.26 0.28 0.30 0.32
 for k in 0.02 0.04 0.06 0.08 0.10 0.12 0.14 0.16 0.18 0.20 0.22 0.24 0.26 0.28 0.30 0.32 0.34 0.36 0.38 0.40 0.42 0.44 0.46 0.48 0.50 0.52 0.54 0.56 0.58 0.6 0.62 0.64 0.66 0.68 0.7
 do
-	./build/Garnet_standalone/gem5.opt -d $out_dir/64c/regular/${routing_algorithm[0]}/${bench_caps[$b]}/vc-${vc_}/bubble-${bubble}/intra-${intraPeriod}_inter-${interPeriod}/inj-${k} configs/example/garnet_synth_traffic.py --topology=Mesh_XY --num-cpus=64 --num-dirs=64 --mesh-rows=8 --network=garnet2.0 --router-latency=1 --sim-cycles=${cycles} --inj-vnet=0 --vcs-per-vnet=${vc_} --sim-type=2 --enable-bn=1 --rand-bb=1 --num-bubble=${bubble} --intra-period=${intraPeriod} --inter-period=${interPeriod} --injectionrate=${k} --synthetic=${bench[$b]} --routing-algorithm=3 &
+	./build/Garnet_standalone/gem5.opt -d $out_dir/64c/regular/${routing_algorithm[0]}/${bench_caps[$b]}/vc-${vc_}/bubble-${bubble}/intra-${intraPeriod}_inter-${interPeriod}/inj-${k} configs/example/garnet_synth_traffic.py --topology=Mesh_XY --num-cpus=64 --num-dirs=64 --mesh-rows=8 --network=garnet2.0 --router-latency=1 --sim-cycles=${cycles} --inj-vnet=0 --vcs-per-vnet=${vc_} --sim-type=2 --enable-bn=1 --rand-bb=1 --num-bubble=${bubble} --intra-period=${intraPeriod} --inter-period=${interPeriod} --injectionrate=${k} --synthetic=${bench[$b]} --routing-algorithm=3 
 done
 #sleep 300
 done
